6f0d7ed897cf8167e47a364509bef0b94c51b32a
Keenan Ng <keenandng@gmail.com>
2020-09-21 16:55:41 -0700

    create new page-header.js, define PageHeader class that takes one parameter, headerElement, assign headerElement to a property in the constructor, create a method named updateAverage that takes one parameter newAverage, which console.logs it for now, add constructor of App class to receive pageHeader as a second parameter, assign that pageHeader parameter to a property in the constructor, query the <header> and assign it to a variable, instantiate a PageHeader class and pass the queried header element as the argument, modify the instantiation of App class to take the PageHeader instance as a second argument

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+var header = document.querySelector('header');
 var table = document.querySelector('table');
+
+var pageHeader = new PageHeader(header);
 var gradeTable = new GradeTable(table);
 var app = new App(gradeTable);
+
 app.start()
