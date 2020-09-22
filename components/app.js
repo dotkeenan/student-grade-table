910f75d831d0853c84b791a39604379a5e2d2b65
Keenan Ng <keenandng@gmail.com>
2020-09-22 11:19:08 -0700

    Added 3 new methods to App.js: deleteGrade(), handleDeleteGradeError(), handleDeleteGradeSuccess(), and bound them all within the constructor

--- components/app.js
+++ components/app.js
@@ -8,6 +8,9 @@ class App {
     this.createGrade = this.createGrade.bind(this);
     this.handleCreateGradeError = this.handleCreateGradeError.bind(this);
     this.handleCreateGradeSuccess = this.handleCreateGradeSuccess.bind(this);
+    this.deleteGrade = this.deleteGrade.bind(this);
+    this.handleDeleteGradeError = this.handleDeleteGradeError.bind(this);
+    this.handleDeleteGradeSuccess = this.handleDeleteGradeSuccess.bind(this);
 
   }
   handleGetGradesError(error)  {
@@ -69,4 +72,18 @@ class App {
   handleCreateGradeSuccess()  {
     this.getGrades();
   }
+
+  deleteGrade(id) {
+    // temporary for now
+    console.log(id);
+  }
+
+  handleDeleteGradeError(error) {
+    console.error(error);
+  }
+
+  handleDeleteGradeSuccess()  {
+    this.getGrades();
+  }
+
 }
